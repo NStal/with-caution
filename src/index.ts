d0f0fbd6dc4c04d1994dce6d5cef7f3fcb8cb111
NStal <nstalmail@gmail.com>
2024-12-25 17:22:32 +0900

    auto bump package version

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@
 export namespace CautionUtil {
     export function never(reason: never): never;
     export function never(sth: never, reason: string): never;
+    export function never<T>(target: T, key: keyof T, reason: string): never;
     export function never(...args: any[]) {
-        if (typeof args[1] == "string") {
+        if (args[2]) {
+            let reason = args[2]
+            let target = args[0]
+            let key = args[1]
+            let value = target?.[key]
+            throw new Error(`Unhandled reason: ${reason} => get value ${value}`)
+        }
+        else if (typeof args[1] == "string") {
             let reason = args[1]
             throw new Error(`Unhandled reason: ${reason} => get value ${args[0]}`)
         } else {
